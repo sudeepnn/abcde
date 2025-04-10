3df4889eb53ec6fd0da063afd8e01d5bbc49659a
sudeepnn <sudeep2002naik@gmail.com>
2025-04-10 12:47:20 +0530

    major update fix1

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { useNavigate, useLocation, Link } from "react-router-dom";
+import { useState } from "react";
+import {  , Link } from "react-router-dom";
 
 interface NavbarProps {
   parentcompanyRef: React.RefObject<HTMLDivElement>;
